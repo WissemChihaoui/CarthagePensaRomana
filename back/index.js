97255fa3ab2f48d4d65c2ee1df89e5e8e732ff05
Wissem Chihaoui <chihahoui10@gmail.com>
2024-04-17 16:30:39 +0100

    first commit

---
+++ back/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/myapp', {
+    serverSelectionTimeoutMS: 5000,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
